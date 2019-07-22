68195039ce710fa21e62244339de635ad3739475
Stephanie Zhang <stephanie@chainsafe.io>
2019-07-22 17:18:45 -0400

    use the whiteblock binary

--- scripts/whiteblock.sh
+++ scripts/whiteblock.sh
@@ -5,20 +5,17 @@ set -ev
 JWT=$1
 BRANCH=$2
 
-go get github.com/whiteblock/genesis
-WORKING_DIR=$(pwd)
-cd $GOPATH/src/github.com/whiteblock/genesis
-go build
-cd $WORKING_DIR
+curl 'https://storage.googleapis.com/genesis-public/cli/dev/bin/linux/amd64/whiteblock' --output ./wb
+chmod +x ./wb
 
-wb login $JWT
+./wb login $JWT
 echo "logged into whiteblock"
 
 echo "building gethc node..."
-wb build --git-repo https://github.com/etclabscore/go-ethereum.git --git-repo-branch $BRANCH -n 1 -m 0 -c 0 -y -p0=8570:8545
+./wb build --git-repo https://github.com/etclabscore/go-ethereum.git --git-repo-branch $BRANCH -n 1 -m 0 -c 0 -y -p0=8570:8545
 
 echo "building parity node..."
-wb build append -b parity -n 1 -m 0 -c 0 -y -p1=8571:8545
+./wb build append -b parity -n 1 -m 0 -c 0 -y -p1=8571:8545
 
 echo "pulling latest testing script from github..."
 git clone https://github.com/ChainSafe/Attalus.git
@@ -28,7 +25,7 @@ yarn install
 PK=(whiteblock get account info | jq '.[].privateKey'| sed -e 's/"//g')
 
 echo "running script"
-./attalus --pk $PK && wb done
+./attalus --pk $PK && ./wb done
 
 
 
