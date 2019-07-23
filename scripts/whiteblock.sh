dcea296a9ffe1c0ed3c47abeb7eef50e33bbe890
Stephanie Zhang <stephanie@chainsafe.io>
2019-07-23 11:55:48 -0400

    added whiteblock config/script

---
+++ scripts/whiteblock.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ev
+
+JWT=$1
+BRANCH=$2
+
+curl 'https://storage.googleapis.com/genesis-public/cli/dev/bin/linux/amd64/whiteblock' --output ./wb
+chmod +x ./wb
+
+./wb login $JWT 2265
+
+echo "building gethc node..."
+./wb build --git-repo https://github.com/etclabscore/go-ethereum.git --git-repo-branch $BRANCH -n 1 -m 0 -c 0 -y -p0=8570:8545
+
+echo "building parity node..."
+./wb build append -b parity -n 1 -m 0 -c 0 -y -p1=8571:8545
+
+echo "pulling latest testing script from github..."
+git clone https://github.com/ChainSafe/Attalus.git
+cd ./Attalus
+yarn install
+
+PK=(whiteblock get account info | jq '.[].privateKey'| sed -e 's/"//g')
+
+echo "running script"
+./attalus --pk $PK && ./wb done
