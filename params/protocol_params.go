380cab360d6bf27cc3d5c9ea577c9d3d21ef007c
Austin Abell <austinabell8@gmail.com>
2019-09-19 21:24:30 -0400

    Agharta/eip1014 create2 (#51)
    * Created branch & added new opcode into opcode.go file
    
    * Added create2 function
    
    * Implemented create function for Create2 contracts
    
    * Created branch & added new opcode into opcode.go file
    
    * Added create2 function
    
    * Implemented create function for Create2 contracts
    
    * Finished Create2 opcode & cleaned up comments
    
    * Created branch & added new opcode into opcode.go file
    
    * Added create2 function

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -14,4 +14,5 @@ var (
 
 	MinimumDifficulty = big.NewInt(131072) // The minimum that the difficulty may ever be.
 	CreateDataGas     = big.NewInt(200)
+	Sha3WordGas       = big.NewInt(6)      // Once per word of the SHA3 operation's data.
 )
