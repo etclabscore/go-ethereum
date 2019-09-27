18b3e0b9101ed357a2d7ef716be43fb8fe0deaac
austinabell <austinabell8@gmail.com>
2019-09-26 20:34:09 -0400

    Add back gas consumption

--- core/vm/vm.go
+++ core/vm/vm.go
@@ -224,6 +224,8 @@ func calculateGasAndSize(gasTable *GasTable, env Environment, contract *Contract
 		}
 	case EXTCODESIZE:
 		gas.Set(gasTable.ExtcodeSize)
+	case EXTCODEHASH:
+		gas.Set(gasTable.ExtcodeHash)
 	case BALANCE:
 		gas.Set(gasTable.Balance)
 	case SLOAD:
