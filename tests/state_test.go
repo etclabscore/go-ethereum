d2d7779aa8ade50dbb78be8958e36c4e5adb3b2b
austinabell <austinabell8@gmail.com>
2019-09-26 20:57:31 -0400

    Remove skipped directories

--- tests/state_test.go
+++ tests/state_test.go
@@ -689,17 +689,8 @@ func TestAllETH(t *testing.T) {
 	skipTests["RevertPrecompiledTouch_storage.json/ConstantinopleFix/0"] = "Bug in Test"
 	skipTests["RevertPrecompiledTouch_storage.json/ConstantinopleFix/3"] = "Bug in Test"
 
-	unsupportedDirs := map[string]bool{
-		"stZeroKnowledge":  true,
-		"stZeroKnowledge2": true,
-	}
-
 	for _, dn := range dirNames {
 		dirName := dn[strings.LastIndex(dn, "/")+1 : len(dn)]
-		if unsupportedDirs[dirName] {
-			continue
-		}
-
 		t.Run(dirName, func(t *testing.T) {
 			fns, _ := filepath.Glob(filepath.Join(ethGeneralStateDir, dirName, "*"))
 			runETHTests(t, fns, skipTests)
